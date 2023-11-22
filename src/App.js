ae4d468ce12d36a955e82c930bf2b262b752a19c
unknown <johncarlolipa@gmail.com>
2023-11-22 10:20:37 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import "./App.css";
 import "bulma/css/bulma.css";
 import Navbar from "./components/Navbar";
-import FormField from "./components/FormField";
 import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-      <FormField
-        label="Email"
-        type="email"
-        placeholder="e.g. alexsmith@gmail.com"
-      /> */}
       <SignupForm />
     </div>
   );
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,14 +1,10 @@
-// src/components/SignupForm.js
-
 import React from "react";
-import Navbar from "./Navbar";
 import FormField from "./FormField";
-import "./SignupForm.css"; // Import the CSS file
+import "./SignupForm.css";
 
 function SignupForm() {
   return (
     <div>
-      {/* <Navbar /> */}
       <h1 className="sign-up">Sign Up</h1>
       <form>
         <FormField label="Name" type="text" placeholder="Enter your name" />
